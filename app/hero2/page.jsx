079df2e12e63a67ec6cb6c83512e1315e97ca332
Tarun <tarunappari23@gmail.com>
2025-12-20 04:50:48 +0530

    changed hero2 page

--- app/hero2/page.jsx
+++ app/hero2/page.jsx
@@ -1,32 +1,14 @@
-import Logo from '@/components/common/Logo';
-import Navbar from '@/components/common/Navbar';
-import NavbarVersion2 from '@/components/common/NavbarVersion2';
-import Footer from '@/components/common/Footer';
-import Experience from '@/components/landingpage/Experience';
-import HeroSectionVersion3 from '@/components/landingpage/HeroSectionVersion3';
-import Ingredients from '@/components/landingpage/Ingredients';
-import UniqueTaste from '@/components/landingpage/UniqueTaste';
-import Why from '@/components/landingpage/Why';
-import React from 'react'
-import Contact from '@/components/landingpage/Contact';
-import Faq from '@/components/landingpage/Faq';
+import Navbar from "@/components/common/Navbar";
+import LandingSection from "@/components/landingpage/HeroSection";
+import { HeroSection } from "@/components/landingpage/HeroVersion2";
+import { Header } from "@/components/ui/header-2";
 
-const page = () => {
+
+export default function Home() {
   return (
-    <div className='relative'>
-      <Logo />
-      <NavbarVersion2 />
-      {/* <Navbar /> */}
-      <HeroSectionVersion3 />
-      <Experience />
-      <UniqueTaste />
-      <Ingredients />
-      <Why />
-      <Faq />
-      <Contact />
-      <Footer />
+    <div className="relative ">
+      <Navbar />
+      <HeroSection />
     </div>
   )
 }
-
-export default page;
--- app/page.jsx
+++ app/page.jsx
@@ -1,14 +1,32 @@
-import Navbar from "@/components/common/Navbar";
-import LandingSection from "@/components/landingpage/HeroSection";
-import { HeroSection } from "@/components/landingpage/HeroVersion2";
-import { Header } from "@/components/ui/header-2";
+import Logo from '@/components/common/Logo';
+import Navbar from '@/components/common/Navbar';
+import NavbarVersion2 from '@/components/common/NavbarVersion2';
+import Footer from '@/components/common/Footer';
+import Experience from '@/components/landingpage/Experience';
+import HeroSectionVersion3 from '@/components/landingpage/HeroSectionVersion3';
+import Ingredients from '@/components/landingpage/Ingredients';
+import UniqueTaste from '@/components/landingpage/UniqueTaste';
+import Why from '@/components/landingpage/Why';
+import React from 'react'
+import Contact from '@/components/landingpage/Contact';
+import Faq from '@/components/landingpage/Faq';
 
-
-export default function Home() {
+const page = () => {
   return (
-    <div className="relative ">
-      <Navbar />
-      <HeroSection />
+    <div className='relative'>
+      <Logo />
+      <NavbarVersion2 />
+      {/* <Navbar /> */}
+      <HeroSectionVersion3 />
+      <Experience />
+      <UniqueTaste />
+      <Ingredients />
+      <Why />
+      <Faq />
+      <Contact />
+      <Footer />
     </div>
   )
 }
+
+export default page;
